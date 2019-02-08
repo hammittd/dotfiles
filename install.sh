0e475f39ffe5f7f340d155d20f9cb5f81597923e
Derek Hammitt <derekrhammitt@gmail.com>
2019-02-07 23:10:38 -0600

    Add vimrc and theme, update install script
    The vim dir will is designed to be symlinked to .vim in ~/. The
    install script will now check if any of the files already exist
    in ~/, and if they do it will remove them and symlink the files
    in the current working directory. If the config files do not exist
    in ~/, it will create the symlinks.

--- install.sh
+++ install.sh
@@ -2,6 +2,39 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-ln -s ${PWD}/bashrc ~/.bashrc
-ln -s ${PWD}/zshrc ~/.zshrc
-ln -s ${PWD}/tmux.conf ~/.tmux.conf
+
+if [ -e ~/.bashrc ];
+    then
+        rm ~/.bashrc && ln -s ${PWD}/bashrc ~/.bashrc
+    else
+	ln -s ${PWD}/bashrc ~/.bashrc
+fi
+
+if [ -e ~/.zshrc ];
+    then
+        rm ~/.zshrc && ln -s ${PWD}/zshrc ~/.zshrc
+    else
+	ln -s ${PWD}/zshrc ~/.zshrc
+
+fi
+
+if [ -e ~/.tmux.conf ];
+    then
+        rm ~/.tmux.conf && ln -s ${PWD}/tmux.conf ~/.tmux.conf
+    else
+	ln -s ${PWD}/tmux.conf ~/.tmux.conf
+fi
+
+if [ -d ~/.vim ];
+    then
+        rm -R ~/.vim && ln -s ${PWD}/vim/ ~/.vim
+    else
+        ln -s ${PWD}/vim/ ~/.vim
+fi
+
+if [ -e ~/.vimrc  ];
+    then
+        rm ~/.vimrc && ln -s ${PWD}/vimrc ~/.vimrc
+    else
+        ln -s ${PWD}/vimrc ~/.vimrc
+fi
---
+++ vimrc
@@ -0,0 +1,15 @@
+" Map <C-W><C-direction> to <C-direction>
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-H> <C-W><C-H>
+nnoremap <C-L> <C-W><C-L>
+
+" Themes
+colorscheme tomorrow-night
+
+" Filetypes and syntax highlights
+filetype on
+syntax on
+
+" Tabs
+set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
