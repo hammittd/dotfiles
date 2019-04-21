50239f62462770fc9c71003ec5a141d82683f06c
Derek Hammitt <derekrhammitt@gmail.com>
2019-04-20 23:39:58 -0500

    tmux configuration updates, bootstrap script

--- configure.sh
+++ configure.sh
@@ -2,51 +2,44 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-if [[ -e ~/.aliases ]];
-    then
-        rm ~/.aliases && ln -s ${PWD}/aliases ~/.aliases
-    else
-        ln -s ${PWD}/aliases ~/.aliases
+if [[ -L ~/.aliases ]]; then
+    rm ~/.aliases && ln -s ${PWD}/aliases ~/.aliases
+else
+    ln -s ${PWD}/aliases ~/.aliases
 fi
 
-if [[ -e ~/.bashrc ]];
-    then
-        rm ~/.bashrc && ln -s ${PWD}/bashrc ~/.bashrc
-    else
+if [[ -L ~/.bashrc ]]; then
+    rm ~/.bashrc && ln -s ${PWD}/bashrc ~/.bashrc
+else
 	ln -s ${PWD}/bashrc ~/.bashrc
 fi
 
-if [[ -e ~/.tmux.conf ]];
-    then
-        rm ~/.tmux.conf && ln -s ${PWD}/tmux.conf ~/.tmux.conf
-    else
-	ln -s ${PWD}/tmux.conf ~/.tmux.conf
+if [[ -L ~/.tmux.conf ]]; then
+    rm ~/.tmux.conf && ln -s ${PWD}/tmux ~/.tmux
+else
+	ln -s ${PWD}/tmux ~/.tmux.conf
 fi
 
-if [[ -d ~/.vim ]];
-    then
-        rm -R ~/.vim && ln -s ${PWD}/vim/ ~/.vim
-    else
-        ln -s ${PWD}/vim/ ~/.vim
+if [[ -L ~/.vim ]]; then
+    rm -R ~/.vim && ln -s ${PWD}/vim/ ~/.vim
+else
+    ln -s ${PWD}/vim/ ~/.vim
 fi
 
-if [[ -e ~/.vimrc  ]];
-    then
-        rm ~/.vimrc && ln -s ${PWD}/vimrc ~/.vimrc
-    else
-        ln -s ${PWD}/vimrc ~/.vimrc
+if [[ -L ~/.vimrc  ]]; then
+    rm ~/.vimrc && ln -s ${PWD}/vimrc ~/.vimrc
+else
+    ln -s ${PWD}/vimrc ~/.vimrc
 fi
 
-if [[ -e ~/.zshrc.local  ]];
-    then
-        rm ~/.zshrc.local && ln -s ${PWD}/zshrc.local ~/.zshrc.local
-    else
-        ln -s ${PWD}/zshrc.local ~/.zshrc.local
+if [[ -L ~/.zshrc.local  ]]; then
+    rm ~/.zshrc.local && ln -s ${PWD}/zshrc.local ~/.zshrc.local
+else
+    ln -s ${PWD}/zshrc.local ~/.zshrc.local
 fi
 
-if [[ -e ~/.profile ]];
-    then
-        rm ~/.profile && ln -s ${PWD}/profile.symlink ~/.profile
-    else
-        ln -s ${PWD}/profile.symlink ~/.profile
+if [[ -L ~/.profile ]]; then
+    rm ~/.profile && ln -s ${PWD}/profile.symlink ~/.profile
+else
+    ln -s ${PWD}/profile.symlink ~/.profile
 fi
